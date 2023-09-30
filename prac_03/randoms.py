0534ae51db70bd1d3d8fa24b6136a0f72138b87e
Silent Neko <salvingeorge17@gmail.com>
2023-09-30 15:18:22 +1000

    Add question answers

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+# Q1: The smallest and largest numbers that can be seen are 5 and 20 inclusive.
+# Q2: The smallest and largest numbers that can be seen are 3 and 8.
+# Q3: The smallest and largest numbers that can be seen are 2.5 and 5.5 inclusive.
+
+import random
+print(random.randint(1,100))
