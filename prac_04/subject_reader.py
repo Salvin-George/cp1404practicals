c09269fcf21502c49c5babf3561a506dc3c90fe1
Silent Neko <salvingeorge17@gmail.com>
2023-10-07 21:26:42 +1000

    Add function refactoring to main.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,12 +7,13 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = get_data()
-    print(data)
+    subjects = get_data(FILENAME)
+    print_subjects(subjects)
 
 
-def get_data():
+def get_data(FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subjects = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +23,16 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(parts)
         print("----------")
     input_file.close()
+
+    return subjects
+
+
+def print_subjects(subjects):
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
 
 
 main()
