2248f3e14aa42bb843382acb462e18961143547b
Silent Neko <salvingeorge17@gmail.com>
2023-10-07 21:35:56 +1000

    Answer practical questions (part 2).

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,3 +10,10 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {(sum(numbers)/len(numbers))}")
+
+# Part 2
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access Granted")
+print("Access Denied")
