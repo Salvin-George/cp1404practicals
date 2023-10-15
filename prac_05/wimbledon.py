d34b1b8ad3f707187c2b873b66ab72c9c14708d5
Silent Neko <salvingeorge17@gmail.com>
2023-10-15 15:17:25 +1000

    Add data extraction and sorting to champion count and country set.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,6 +9,8 @@ FILENAME = "wimbledon.csv"
 
 def main():
     formatted_data = read_file(FILENAME)
+    champion_count, countries = get_champion_stats(formatted_data)
+    display_countries(champion_count, countries)
 
 
 def read_file(FILENAME):
@@ -19,4 +21,16 @@ def read_file(FILENAME):
                 champion, country = line.split(",")
                 data.append([champion, country])
     return data
+
+
+def get_champion_stats(formatted_data):
+    champion_count = {}
+    country_set = set()
+    for champion, country in formatted_data:
+        champion_count[champion] = champion_count.get(champion, 0) + 1
+        country_set.add(country)
+    return champion_count, sorted(country_set)
+
+
+
 
