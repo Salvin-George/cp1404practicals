caf8268250eba519a860977925600ab5cef0bd7c
Silent Neko <salvingeorge17@gmail.com>
2023-10-30 16:51:33 +1000

    Finish part 2 of practical question.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,3 +11,6 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+
+ProgrammingLanguage.print_dyanmic_programs(visual_basic)
+
--- prac_06/programing_language.py
+++ prac_06/programing_language.py
@@ -15,7 +15,21 @@ class ProgrammingLanguage:
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
     def is_dynamic(self):
         """Determines if programing is dynamically typed."""
         return self.typing == "Dynamic"
 
+    def print_dyanmic_programs(self):
+        python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+        ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+        visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+        languages = [python, ruby, visual_basic]
+
+        print("The dynamically typed languages are:")
+        for language in languages:
+            if language.is_dynamic():
+                print(language.name)
