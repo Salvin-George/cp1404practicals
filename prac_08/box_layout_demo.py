08a5f745a26777521d0b33c2f073791f1d9bcc39
Silent Neko <salvingeorge17@gmail.com>
2023-11-12 16:57:37 +1000

    Complete part 2 of practical.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,3 +1,6 @@
+"""
+Practical 08
+"""
 from kivy.app import App
 from kivy.lang import Builder
 
@@ -7,6 +10,12 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_greet(self):
+        self.root.ids.output_label.text = 'Hello '
 
 
 BoxLayoutDemo().run()
