09940d73084458b1b9d7108e84a05b91b493c49b
Silent Neko <salvingeorge17@gmail.com>
2023-11-12 16:58:46 +1000

    Refactor module doc strings.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -6,15 +6,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Constructs App class"""
+
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Event handler for greeting."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def clear_greet(self):
+        """Event handler for clearing greeting.."""
         self.root.ids.output_label.text = 'Hello '
 
 
