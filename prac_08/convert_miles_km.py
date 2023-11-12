f3365dad7ac9779b244742df02575c7ab934ae14
Silent Neko <salvingeorge17@gmail.com>
2023-11-12 20:40:35 +1000

    Complete part 2 of practical.

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,43 @@
+"""
+Prac 07
+Estimate: 25mins
+Actual:
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILESTOKM = 1.60934
+
+
+class MilestoKmApp(App):
+    """Kivy app to convert from miles to Kilometers"""
+    output = StringProperty()
+
+    def build(self):
+        """Builds Kivy App"""
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.output = "Type in the field & press Enter"
+        return self.root
+
+    def get_valid_input(self):
+        """Error checks input of app"""
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+    def handle_calculation(self):
+        """Changes display to show calculations"""
+        self.output = str(self.get_valid_input() * MILESTOKM)
+
+    def handle_increment(self, change):
+        """Increases current values by +/- 1"""
+        self.root.ids.input_miles.text = str(self.get_valid_input() + change)
+        self.handle_calculation()
+
+
+MilestoKmApp().run()
