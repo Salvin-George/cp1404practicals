376f5b72afbfc2bf64f1a2594aec010f4735ed91
Silent Neko <salvingeorge17@gmail.com>
2023-11-19 11:58:45 +1000

    complete part 1 of practical

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,19 @@
+"""
+Taxi class test
+Estimate: 10mins
+Actual:
+"""
+import prac_09.taxi
+
+
+def main():
+    """Test taxi class."""
+    my_taxi = prac_09.taxi.Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
