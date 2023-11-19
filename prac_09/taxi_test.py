6d7c9b32d2d8bf14c92955f9f980ef768f8b26c6
Silent Neko <salvingeorge17@gmail.com>
2023-11-19 11:59:10 +1000

    complete part 2 of practical

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ import prac_09.taxi
 
 def main():
     """Test taxi class."""
-    my_taxi = prac_09.taxi.Taxi("Prius 1", 100, 1.23)
+    my_taxi = prac_09.taxi.Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
