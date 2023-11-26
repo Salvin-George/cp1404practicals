84a9debb3a258d79b4d826e11ab540eea50c7270
Silent Neko <salvingeorge17@gmail.com>
2023-11-26 20:09:33 +1000

    Finish prac requirements.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,3 +4,12 @@ Esitmate: 7mins
 Actual:
 """
 
+import wikipedia
+
+search = input("Search: ")
+while search:
+    try:
+        summary = wikipedia.summary(search)
+        print(summary)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(f"Ambiguous term. Options: {e.options}")
