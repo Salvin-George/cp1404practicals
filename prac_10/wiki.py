8eb99d5489a5c2b496df014ca8cdfe1c31eaafde
Silent Neko <salvingeorge17@gmail.com>
2023-11-26 20:11:06 +1000

    Add .summary arguments.

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -9,7 +9,7 @@ import wikipedia
 search = input("Search: ")
 while search:
     try:
-        summary = wikipedia.summary(search)
+        summary = wikipedia.summary(search, autosuggest=False)
         print(summary)
     except wikipedia.exceptions.DisambiguationError as e:
         print(f"Ambiguous term. Options: {e.options}")
